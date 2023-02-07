fcb1e6ed7e4b281e296cd55c08548d1bba5a2ea2
Natnael Demelash <natidemelash074@gmail.com>
2023-02-07 18:51:00 +0300

    Todo header setup

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterView } from "vue-router";
+import TheHeader from "./components/TheHeader.vue";
+</script>
+
+<template>
+  <TheHeader />
+  <RouterView />
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Abel&family=Dosis:wght@300;400;500;700&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Abel", sans-serif;
+}
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+</style>
